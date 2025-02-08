3bb1cb16e18a806738be27fdcf590af57cb46962
Ali Mousavi <96045566+Ali-Primer@users.noreply.github.com>
2025-02-09 00:05:40 +0330

    ui done

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,33 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div class="w-full h-screen bg-black"></div>
-      
+      <div className="w-full h-screen bg-slate-800 flex justify-center items-center font-poppins">
+        <div className="flex w-1/2 flex-col justify-evenly items-center">
+          <p className="text-yellow-300 text-5xl font-dancing">Tic Tac Toe</p>
+          <div className="grid h-96 w-96 grid-cols-3 grid-rows-3 gap-4 mt-6">
+            {Array.from({ length: 9 }).map((_, i) => (
+              <div key={i} className="bg-white/30 rounded-md cursor-pointer backdrop-blur-md hover:bg-white/60 transition"></div>
+            ))}
+            {/* <div className="bg-white/30 h-24 w-24 rounded-md backdrop-blur-md"></div>
+              <div className="bg-white/30 h-24 w-24 rounded-md backdrop-blur-md"></div>
+              <div className="bg-white/30 h-24 w-24 rounded-md backdrop-blur-md"></div>
+              <div className="bg-white/30 h-24 w-24 rounded-md backdrop-blur-md"></div>
+              <div className="bg-white/30 h-24 w-24 rounded-md backdrop-blur-md"></div>
+              <div className="bg-white/30 h-24 w-24 rounded-md backdrop-blur-md"></div>
+              <div className="bg-white/30 h-24 w-24 rounded-md backdrop-blur-md"></div> */}
+          </div>
+          <div className="mt-6 flex justify-between w-2/3">
+          <div className="text-lg text-white rounded px-8 py-2 border cursor-pointer hover:bg-red-600 hover:border-red-600 transition">
+            Reset
+          </div>
+          <div className="text-lg bg-red-600 cursor-pointer text-slate-50 rounded px-8 py-2 hover:bg-green-600 transition">
+            Auto-Mode
+          </div>
+          </div>
+        </div>
+        <div>
+        </div>
+      </div>
     </>
   );
 }
